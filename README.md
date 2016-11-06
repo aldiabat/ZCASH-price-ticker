02939132a1e38ca24fc01a493a4fefc5ae8ba86f
johnerfx <johnerfx@github.com>
2016-11-06 21:35:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<b>ZCASH price ticker</b></br>
+Chrome web store link:
+https://chrome.google.com/webstore/detail/zcash-price-ticker/okgakcilgbglfcbaclakjinnhjjnbbhf
+
+![ZCASH price ticker - screenshot](https://raw.githubusercontent.com/johnerfx/ZCASH-price-ticker/master/screenshot.png)
+
+This extension shows ZCASH value priced in Bitcoin and American Dollars(BTC, USD).</br>
+ZCASH (https://z.cash/) is a cryptocurrency that offers total payment confidentiality, while still maintaining a decentralized network using a public blockchain.
+Values are refreshed every 4 minutes.</br>
+Values are based on data provided by 'www.coinmarketcap.com' API and are most likely appropriate, but don't have to be.</br>
+No special permissions are needed to install this extension.
+Extension icon - ZCASH GitHub repository (https://github.com/zcash/logos/blob/master/yellow-zcash-logo.png).</br></br>
+Tips appreciated:</br>
+ZCASH: t1cXNWbAjqPaW2vxfareuFBXj5DBE9yoyNe </br>
+BTC: 1Cf4rZcqhedQhNuRZQQKhn1cv9mTxj1K6Z </br>
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/johnerfx/ZCASH-price-ticker/blob/master/LICENSE)
---
+++ ticker.js
@@ -0,0 +1,55 @@
+//by johnerfx (MIT Licensed)
+function checkTicker() {
+  var ticker = new XMLHttpRequest();
+	ticker.open("GET", "https://api.coinmarketcap.com/v1/ticker/", true);
+	ticker.onreadystatechange = function() {
+	if (ticker.readyState == 4 && ticker.status == 200) {
+		var jsonresponse=JSON.parse(ticker.responseText);
+		for (i=0;i<jsonresponse.length;i++) {
+			if (jsonresponse[i]['id']=='zcash') {
+				chrome.extension.getBackgroundPage().console.log(window.navigator.language);
+				var zcash_btc=jsonresponse[i]['price_btc'];
+				var zcash_usd=jsonresponse[i]['price_usd'];
+				var zcash_percent1h=jsonresponse[i]['percent_change_1h'];
+				var zcash_percent24h=jsonresponse[i]['percent_change_24h'];
+				var zcash_percent7d=jsonresponse[i]['percent_change_7d'];
+				var zcash_marketcap=jsonresponse[i]['market_cap_usd'];
+				var zcash_volume=jsonresponse[i]['24h_volume_usd'];
+			}
+		}
+		chrome.browserAction.setBadgeText({text: parseFloat(zcash_usd).toFixed(1)});
+		var lines=["ZCASH price ticker (price taken from 'www.coinmarketcap.com')\n"];
+    var options = { style: 'currency', currency: 'USD'};
+		lines.push("\n");
+		lines.push("ZCASH "+FixIfNotNull(zcash_btc,8)+"BTC\n");
+		lines.push("ZCASH "+FixIfNotNull(zcash_usd,4)+"$\n");
+		lines.push("ZCASH Marketcap "+Number(zcash_marketcap).toLocaleString(window.navigator.language,options)+"\n");
+		lines.push("ZCASH 24h change "+FixIfNotNull(zcash_percent24h,2)+"%\n");
+		lines.push("ZCASH 24h Volume "+Number(zcash_volume).toLocaleString(window.navigator.language,options)+"\n");
+		lines.push("\n");
+		lines.push(Date());
+		var title_lines="";
+		for (j=0;j<lines.length;j++) {
+			title_lines=title_lines+lines[j];
+		}
+		chrome.browserAction.setTitle({title:title_lines});
+		}
+	}
+ticker.send();
+}
+function FixIfNotNull(variable,decimals) {
+	//Function passess variable toFixed only if it is not null (edge case)
+	//then returns fixed_var as string
+	var fixed_var="UNAVAILABLE";
+	if (variable) {
+		fixed_var=parseFloat(variable).toFixed(decimals);
+	}
+	return fixed_var.toString()
+}
+chrome.browserAction.onClicked.addListener(function(activeTab)
+    {
+      var newURL = "http://coinmarketcap.com/currencies/zcash/";
+      chrome.tabs.create({ url: newURL });
+    });
+window.setInterval(checkTicker, 240000); //4 minutes
+checkTicker();
